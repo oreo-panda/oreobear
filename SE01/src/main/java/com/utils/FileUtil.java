d5dcd954af700a5d46c8e87c12990a2aa55fafd2
oreo-panda <1091567184@qq.com>
2022-09-21 12:22:35 +0800

    11111

---
+++ SE01/src/main/java/com/utils/FileUtil.java
@@ -0,0 +1,31 @@
+package com.utils;
+
+import cn.hutool.core.io.file.FileReader;
+import cn.hutool.core.io.file.FileWriter;
+import com.exception.FileException;
+
+public class FileUtil {
+    public static String readFile(String filepath) throws FileException{
+        if(filepath==null ||"".equals(filepath)){
+            throw new FileException("文件路径为空");
+        }
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(filepath);
+        }catch (Exception e){
+            throw new FileException("文件为空");
+        }
+
+        String result = fileReader.readString();
+        return result;
+    }
+    public static Boolean writeFile(String filepath,String content) {
+        FileWriter writer = new FileWriter(filepath);
+        try {
+            writer.write(content);
+        }catch (Exception e){
+            System.out.println("答案文件输出异常，答案为："+content);
+        }
+        return true;
+    }
+}
